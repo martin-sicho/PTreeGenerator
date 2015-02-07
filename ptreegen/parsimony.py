c67907cd054586e60d8eb0ab7ab58a0b5cd8d52f
martin-sicho <sichom@vscht.cz>
2015-02-07 18:13:17 +0100

    started implementing the LargeParsimony class - search for optimal quartets is now implemented

--- ptreegen/parsimony.py
+++ ptreegen/parsimony.py
@@ -1,3 +1,7 @@
+import copy
+
+from ete2 import Tree
+from Bio.Align import MultipleSeqAlignment
 
 class SmallParsimony:
 
@@ -7,12 +11,12 @@ class SmallParsimony:
         self._seqencesDict = {x.id : x.seq for x in list(alignment)}
         leaf_names = set(tree.get_leaf_names())
         if len(self._seqencesDict) != len(leaf_names):
-            raise RuntimeError("Number of sequnces in alignment:\n" + alignment
-                               + "\ndoes not match the number of leaves in tree:\n" + tree)
+            raise RuntimeError("Number of sequnces in alignment:\n" + repr(alignment)
+                               + "\ndoes not match the number of leaves in tree:\n" + repr(tree))
         for seq_id in self._seqencesDict:
             if seq_id not in leaf_names:
                 raise RuntimeError("Sequence ID (" + seq_id
-                                   + ") does not match any leaf in tree:\n" + tree)
+                                   + ") does not match any leaf in tree:\n" + repr(tree))
 
         self._treeCharacterDict = dict()
         self._cost = 0
@@ -47,3 +51,60 @@ class SmallParsimony:
                 self._treeCharacterDict[node] = character_set
                 self._cost += 1
 
+class LargeParsimony:
+
+    def __init__(self, alignment):
+        self._alignment = alignment
+        self._sequencesDict = {x.id : x.seq for x in list(self._alignment)}
+        quartet_combinations = []
+        for combination in LargeParsimony.uniqueCombinationsGenerator(self._sequencesDict.keys(), 4):
+            quartet_combinations.append(combination)
+
+        self._optimalQuartets = dict()
+        self.saveOptimalQuartets(quartet_combinations)
+        print quartet_combinations
+        print self._optimalQuartets.keys()
+
+    def saveOptimalQuartets(self, quartets):
+        for quartet in quartets:
+            quartet_id = "".join(quartet)
+            quartet_copy = copy.deepcopy(quartet)
+            assert quartet_id not in self._optimalQuartets.keys()
+            trees = [self.treeFromQuartet(quartet_copy)]
+            for i in range(0,2):
+                temp = quartet_copy[i]
+                quartet_copy[i] = quartet_copy[2]
+                quartet_copy[2] = temp
+                trees.append(self.treeFromQuartet(quartet_copy))
+            min_cost = float("inf")
+            for tree in trees:
+                alignment = MultipleSeqAlignment([])
+                for record in self._alignment:
+                    if record.id in quartet_copy:
+                        alignment.append(record)
+                small_parsimony = SmallParsimony(tree, alignment)
+                if small_parsimony.cost < min_cost:
+                    min_cost = small_parsimony.cost
+                    self._optimalQuartets[quartet_id] = tree # TODO: try better representation
+
+    def treeFromQuartet(self, quartet):
+        root = Tree()
+        root.name = "root"
+        left = root.add_child(name="Left")
+        left.add_child(name=quartet[0])
+        left.add_child(name=quartet[1])
+        right = root.add_child(name="Right")
+        right.add_child(name=quartet[2])
+        right.add_child(name=quartet[3])
+        for desc in root.iter_descendants():
+            desc.dist = 0
+        return root
+
+    @staticmethod
+    def uniqueCombinationsGenerator(items, n):
+            if not n:
+                yield []
+            else:
+                for i in xrange(len(items)):
+                    for comb in LargeParsimony.uniqueCombinationsGenerator(items[i+1:], n-1):
+                        yield [items[i]] + comb
--- tests/character_based_test.py
+++ tests/character_based_test.py
@@ -6,7 +6,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 
-from ptreegen.parsimony import SmallParsimony
+from ptreegen.parsimony import SmallParsimony, LargeParsimony
 
 
 class TestSmallParsimony(unittest.TestCase):
@@ -22,17 +22,18 @@ class TestSmallParsimony(unittest.TestCase):
         node = root.add_child(name="Right")
         node.add_child(name="Gamma")
         node.add_child(name="Delta")
+        for desc in tree.iter_descendants():
+            desc.dist = 0
+
         ts = TreeStyle()
         ts.show_leaf_name = True
         ts.show_branch_length = False
-        # ts.mode = "c"
+        ts.mode = "c"
         ts.arc_start = 0
         ts.arc_span = 360
+
         self.circular_style = ts
-        tree.set_style(ts)
-        # tree.show()
-        # tree.show(tree_style=ts)
-        self.tree = tree
+        self.exampleTree = tree
         self.alignment = MultipleSeqAlignment(
             [
                 SeqRecord(Seq("AAG", generic_dna), id="Alpha"),
@@ -43,9 +44,25 @@ class TestSmallParsimony(unittest.TestCase):
         )
 
     def test_solve(self):
-        # self.tree.show()
-        parsimony = SmallParsimony(self.tree, self.alignment)
+        parsimony = SmallParsimony(self.exampleTree, self.alignment)
         self.assertEqual(parsimony.cost, 4)
+        # self.exampleTree.show(tree_style=self.circular_style)
+
+class testLargeParsimony(unittest.TestCase):
+
+    def setUp(self):
+        self.alignment = MultipleSeqAlignment(
+            [
+                SeqRecord(Seq("AAG", generic_dna), id="Alpha"),
+                SeqRecord(Seq("AGA", generic_dna), id="Beta"),
+                SeqRecord(Seq("AAA", generic_dna), id="Gamma"),
+                SeqRecord(Seq("GGA", generic_dna), id="Delta"),
+                SeqRecord(Seq("TTA", generic_dna), id="Epsilon"),
+            ]
+        )
+
+    def test_solve(self):
+        parsimony = LargeParsimony(self.alignment)
 
 
 if __name__ == '__main__':
